aee4a0dba81fabf7ca0ad16ea867e8b9a5eefae1
yumenghu <Menghu.Yu@partner.bmwgroup.com>
2023-08-30 17:45:01 +0800

    add docker-compose

---
+++ docker/mysql/init/ init.sql
@@ -0,0 +1,6 @@
+CREATE TABLE test (
+                      id INT PRIMARY KEY AUTO_INCREMENT,
+                      name VARCHAR(255),
+                      age INT,
+                      email VARCHAR(255)
+);
